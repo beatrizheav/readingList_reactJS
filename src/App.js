88344f0754b868cabffc9f9bc289872f1c9ebb98
Beatriz Avila <beatriz.heav@gmail.com>
2024-09-02 16:38:43 -0600

    book component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Title from './components/titleComponent.tsx';
+import "./styles/App.css";
+import Title from "./components/Text/Title/titleComponent.tsx";
+import Book from "./components/Book/bookComponent.tsx"
 
 function App() {
   return (
     <div className="App-header">
-        <Title text='Hola mundo'/>
+      <Title text="Books" />
+      <Book link="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1566425108i/33.jpg" title="El Señor de los Anillos"/>
     </div>
   );
 }
