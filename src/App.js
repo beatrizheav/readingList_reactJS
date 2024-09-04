3e0adea90a09bba8cbc758f56eaf266e1baa0835
Beatriz Avila <beatriz.heav@gmail.com>
2024-09-04 09:13:56 -0600

    Subtitle component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./styles/App.css";
 import Title from "./components/Text/Title/titleComponent.tsx";
-import Book from "./components/Book/bookComponent.tsx"
+import Subtitle from "./components/Text/Subtitle/subtitleComponent.tsx";
+import Book from "./components/Book/bookComponent.tsx";
 
 function App() {
   return (
     <div className="App-header">
       <Title text="Books" />
-      <Book link="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1566425108i/33.jpg" title="El Señor de los Anillos"/>
+      <Subtitle text="6 libros disponibles" />
+      <Book
+        link="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1566425108i/33.jpg"
+        title="El Señor de los Anillos"
+      />
     </div>
   );
 }
